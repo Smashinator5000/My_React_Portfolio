6871b38fddbd8afb1ceca152ebf1d69559b4ea57
Ashley Bartholomew <ashley.52389@gmail.com>
2021-03-10 23:24:16 -0700

    deploying

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from 'react';
 import './app.css';
 // import { BrowserRouter as Router, Route } from "react-router-dom"
-import Navbar from "../../myreactportfolio/src/components/navbar";
-import Header from "../../myreactportfolio/src/components/header";
-import AboutMe from "../../myreactportfolio/src/components/aboutme";
-import Experience from "../../myreactportfolio/src/components/experience";
-import Projects from '../../myreactportfolio/src/components/projects';
-import Contact from '../../myreactportfolio/src/components/contact';
-import Footer from '../../myreactportfolio/src/components/footer';
+import Navbar from "../../My_React_Portfolio/src/components/navbar";
+import Header from "../../My_React_Portfolio/src/components/header";
+import AboutMe from "../../My_React_Portfolio/src/components/aboutme";
+import Experience from "../../My_React_Portfolio/src/components/experience";
+import Projects from '../../My_React_Portfolio/src/components/projects';
+import Contact from '../../My_React_Portfolio/src/components/contact';
+import Footer from '../../My_React_Portfolio/src/components/footer';
 
 
 function App() {
